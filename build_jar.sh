9f50444c198cb25614cb9c1ca36cb6c4e58a4a64
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-10-20 09:34:22 -0300

    atualizando repo com idéia para pegar ultima tag do git

--- build_jar.sh
+++ build_jar.sh
@@ -1,1 +1,5 @@
-USER=root PASSWORD=root DATABASE_URL='mysql://localhost:3306/SpringWebYoutubeTest?useTimezone=true&serverTimezone=UTC'  ./mvnw package
+export USER='root' 
+export PASSWORD='root' 
+export DATABASE_URL='mysql://localhost:3306/SpringWebYoutubeTest?useTimezone=true&serverTimezone=UTC'
+
+mvn package
